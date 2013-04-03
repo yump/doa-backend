5b629236d980060e1be88139e2bf43cada171544
Russell Haley <rhaley@tamu.edu>
2013-04-03 04:16:47 -0500

    Minor restructuring.

--- music/estimator.py
+++ music/estimator.py
@@ -1,6 +1,7 @@
-class MusicEstimator
+class estimator
 	
 	"""A class to estimate direction of arrival (DOA) using the Multiple
 	SIgnal Classification algorithm."""
 
-	def __init__(self)
+	def __init__(self):
+		pass
