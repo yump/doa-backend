0fbf9a95bff3be7c19cab1e739ba162e1a639ab5
Russell Haley <rhaley@tamu.edu>
2013-04-11 22:25:17 -0500

    Remove extraneous file that was actually a remnant of the python MUSIC implementation.

--- linalg_extra.py
+++
@@ -1,8 +0,0 @@
-import scipy as sp
-
-def eigsort(eigresult):
-	"""Sort the output of scipy.linalg.eig() in terms of 
-	eignevalue magnitude"""
-
-	ix = sp.argsort(abs(eigresult[0]))
-	return ( eigresult[0][ix], eigresult[1][:,ix] )
